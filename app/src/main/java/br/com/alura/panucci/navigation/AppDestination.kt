72018b5567d303cd6d35a41e14ddedf25aaf0fbb
Pablo Pinheiro <pablo-pinheiro@hotmail.com>
2023-02-23 19:49:41 -0300

    Removido arquivo

--- app/src/main/java/br/com/alura/panucci/navigation/AppDestination.kt
+++
@@ -1,9 +0,0 @@
-package br.com.alura.panucci.navigation
-
-sealed class AppDestination(val route: String){
-    object HighLight: AppDestination("highlight")
-    object Menu: AppDestination("menu")
-    object Drinks: AppDestination("drinks")
-    object ProductDetails: AppDestination("productDetails")
-    object Checkout: AppDestination("checkout")
-}
