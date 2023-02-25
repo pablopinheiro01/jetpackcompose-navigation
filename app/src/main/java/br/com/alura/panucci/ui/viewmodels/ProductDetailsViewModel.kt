8e14a5d683f7d50213dfbbbfa6aecfc31e54d69a
Pablo Pinheiro <pablo-pinheiro@hotmail.com>
2023-02-24 22:02:16 -0300

    simulação de timeout na tela

--- app/src/main/java/br/com/alura/panucci/ui/viewmodels/ProductDetailsViewModel.kt
+++ app/src/main/java/br/com/alura/panucci/ui/viewmodels/ProductDetailsViewModel.kt
@@ -3,13 +3,17 @@ package br.com.alura.panucci.ui.viewmodels
 import android.util.Log
 import androidx.compose.runtime.MutableState
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import br.com.alura.panucci.dao.ProductDao
 import br.com.alura.panucci.ui.uistate.ProductDetailsUiState
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import java.math.BigDecimal
 import javax.security.auth.login.LoginException
+import kotlin.random.Random
 
 class ProductDetailsViewModel(
     private val dao: ProductDao = ProductDao()
@@ -19,13 +23,19 @@ class ProductDetailsViewModel(
     val uiState = _uiState.asStateFlow()
 
     fun findProductById(id: String) {
-        dao.findById(id)?.let { product ->
-            _uiState.update {
-                it.copy(product = product)
+        viewModelScope.launch {
+            //teste de timeout na tela
+            val timeInMillis = Random.nextLong(500, 4000)
+            delay(timeInMillis)
+            dao.findById(id)?.let { product ->
+                _uiState.update {
+                    it.copy(product = product)
+                }
             }
         }
     }
-    fun applyDiscountPromotionalCode(promoCode:String?){
+
+    fun applyDiscountPromotionalCode(promoCode: String?) {
         _uiState.update { state ->
 
             val discount = when (promoCode) {
@@ -40,7 +50,7 @@ class ProductDetailsViewModel(
 
             Log.i(TAG, "applyDiscountPromotionalCode: ${promoCode} value: ${value}")
 
-            val newState = state.copy(product = value?.let{ newValue ->
+            val newState = state.copy(product = value?.let { newValue ->
                 Log.i(TAG, "applyDiscountPromotionalCode: newValue: ${newValue}")
                 state.product.copy(price = newValue)
             })
@@ -52,7 +62,7 @@ class ProductDetailsViewModel(
 
     }
 
-    companion object{
+    companion object {
         const val TAG = "ProductDetailsViewModel"
     }
 
